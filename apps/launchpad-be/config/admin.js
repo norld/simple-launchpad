579d680afac7f4253b3a0b26f8c49999b36e45cb
Made Supardi <grymore@gmail.com>
2022-11-12 20:27:49 +0800

    expired

--- apps/launchpad-be/config/admin.js
+++ apps/launchpad-be/config/admin.js
@@ -1,5 +1,11 @@
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "someRandomLongString"),
+  },
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'f238c05daf33babeaf5e545296f471dc'),
+    options: {
+      expiresIn: "300d",
+    },
+    secret: env("ADMIN_JWT_SECRET", "f238c05daf33babeaf5e545296f471dc"),
   },
 });
